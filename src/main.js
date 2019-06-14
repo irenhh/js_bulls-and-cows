f2f0e128254b69391df67ca4d6dd8bec6a368620
Iryna <iriskag@ukr.net>
2019-06-14 14:41:24 +0300

    Changes were added

--- src/main.js
+++ src/main.js
@@ -2,72 +2,59 @@ let bullCount = 0;
 let cowCount = 0;
 let tryCount = 0;
 let random = randomUniqueDigits();
+let guessesHistory = '';
 
 //gets random number with unique digits
 function getRandom(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function randomUniqueDigits() {
-  let random = [];
-  random.push(getRandom(1, 9));
-  random.push(getRandom(0, 9));
-  for (let i = 1; i < 4; i++) {
-    if (random.indexOf(random[i]) !== i) {
-      do {
-        random.pop();
-        random.push(getRandom(0, 9));
-      } while (random.indexOf(random[i]) !== i);
-    } else {
-      if (random.length >= 4) {
-        break;
-      } else {
-        random.push(getRandom(0, 9))
-      };
-    }
 
+function randomUniqueDigits() {
+  let random = '';
+  let digit = 0;
+  while (random.length < 4) {
+    digit = getRandom(0, 9);
+    if (!random.includes(digit)) {
+      random += digit;
+    }
   }
-  return random = random.join('');
+  return random;
 }
 
 //a single try
-function tries(number) {
+function oneTry(number) {
   number = number.toString();
   console.log(number.toString());
   if (number.length > 4 || number.length < 4) {
     return 'Only 4-digit numbers are accepted';
-  } else {
-    for (let i in number) {
-      if (number.indexOf(number[i]) != i) {
-        console.log('Each digit must be unique');
+  } 
+  for (let i in number) {
+    if (number.indexOf(number[i]) != i) {
+      return 'Each digit must be unique';
+    }
+    if (random.includes(number[i])) {
+      if (random.indexOf(number[i]) === number.indexOf(number[i])) {
+        bullCount++;
       } else {
-        if (random.indexOf(number[i]) !== -1) {
-          if (random.indexOf(number[i]) === number.indexOf(number[i])) {
-            bullCount++;
-          } else {
-            cowCount++;
-          }
-        }
+        cowCount++;
       }
     }
-    tryCount++;
-    if (bullCount === 4) {
-      console.log(`You got 4 bulls in ${tryCount} tries`);
-    } else {
-      console.log(`${bullCount} bulls and ${cowCount} cows, ${tryCount} tries`);
-    }
   }
+  tryCount++;
+  console.log(`${bullCount} bulls and ${cowCount} cows, ${tryCount} tries`);
+  guessesHistory += `${number}: ${bullCount} bulls and ${cowCount} cows, ${tryCount} tries\n`;
 }
 
 //starts a new game
 function newGame() {
-  
   do {
     bullCount = 0;
     cowCount = 0;
-    let number = window.prompt('Type a number');
-    tries(number);
+    let number = prompt(`${guessesHistory} Type a number`);
+    oneTry(number);
   } while (bullCount !== 4);
-  console.log(`Congrats!!! You got ${bullCount} bulls in ${tryCount} tries`);
+  if (bullCount === 4) {
+    console.log(`Congrats!!! You got ${bullCount} bulls in ${tryCount} tries`);
+    alert(`Congrats!!! You got ${bullCount} bulls in ${tryCount} tries`)
+  }
 }
